ed47d1a717c9e0b99b7b4e11cbc82a5818d7aed6
zidong.wang <zidong.wang@qunar.com>
2016-09-20 19:26:20 +0800

    temp

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    /*var dpr = window.devicePixelRatio;
+    alert("物理像素和独立像素比："+dpr);
+    var width = screen.width;
+    var height = screen.height;
+    alert("屏幕宽度："+width+" 屏幕高度："+height);
+    var ww = window.innerWidth;
+    var wh = window.innerHeight;
+    alert("窗口宽度："+ww+" 窗口高度："+wh);*/
+});
